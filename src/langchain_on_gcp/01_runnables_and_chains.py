3c9591848a720bbeeeb7a9eecdda5ef976c62182
Manish Bhobe <mjbhobe@gmail.com>
2026-05-06 14:50:10 +0000

    Email Assistant

--- src/langchain_on_gcp/01_runnables_and_chains.py
+++ src/langchain_on_gcp/01_runnables_and_chains.py
@@ -4,6 +4,7 @@ runnables_and_chains.py - exploring LangChain's runnables & chains
 @Author: Manish Bhobe
 My experiments with LangChain on the GCP. Code is shared for learning purposes only!
 """
+
 import random
 from operator import itemgetter
 from rich.console import Console
@@ -12,7 +13,7 @@ from langchain_core.runnables import RunnableLambda, RunnableParallel
 
 # SEED = 41
 # random.seed(SEED)
-console = Console() # for colorful text on console
+console = Console()  # for colorful text on console
 
 console.print(f"[red]Using Langchain:[/red] {langchain.__version__}")
 
@@ -26,25 +27,30 @@ runnable = RunnableLambda(lambda x: x + 1)
 console.print(f"[sky_blue1]runnable.invoke(1) = [/sky_blue1] {runnable.invoke(1)}")
 console.print(f"[sky_blue1]runnable.invoke(2) = [/sky_blue1] {runnable.invoke(2)}")
 
+
 # runnables can be chained together into a chain, which itself is a runnable
 def get_radius() -> int:
-    """ return a random number between 1 & 15 """
+    """return a random number between 1 & 15"""
     radius = random.randint(1, 15)
     console.print(f"[red]Generated radius: {radius}[/red]")
     return radius
 
-def calculate_circle_area(radius: int) -> float:
-    """ calculate the area of a circle given its radius """
+
+def calculate_circle_area(radius: int) -> str:
+    """calculate the area of a circle given its radius"""
     return f"Area of circle with radius {radius} is {3.14 * radius * radius:.3f}"
 
-def fake_llm(x : int) -> str:
+
+def fake_llm(x: int) -> str:
     return f"Fake LLM says: {x}^2 = {x**2}"
+
 
 # now build a chain like this using LCEL (LangChain Expression Language)
 chain = (
     # have used the funny lambda syntax for the first RunnableLambda as
     # get_radius() does not take a parameter, but invoke()
-    RunnableLambda(lambda _:get_radius()) | RunnableLambda(calculate_circle_area)
+    RunnableLambda(lambda _: get_radius())
+    | RunnableLambda(calculate_circle_area)
 )
 
 # NOTE: the invoke() function requires a parameter!
@@ -53,8 +59,7 @@ console.print(f"[sky_blue1]Response of chain ->[/sky_blue1] {response}")
 
 # you can also run chains in parallel
 parallel_chain = RunnableParallel(
-    step1 = chain,  # same chain as above
-    step2 = RunnableLambda(fake_llm)
+    step1=chain, step2=RunnableLambda(fake_llm)  # same chain as above
 )
 response = parallel_chain.invoke(7)
 console.print(response)
@@ -63,9 +68,6 @@ console.print(response)
 # I have invoked the parallel chain with a single number
 # Normally we would pass a dictionary to the invoke() function
 # In that case, to extract value of key we need to pre-pend itemgetter to the head of our chain
-itemgetter_chain = (
-    itemgetter("x")
-    | RunnableLambda(fake_llm)
-)
-response = itemgetter_chain.invoke({"x" : 20})
+itemgetter_chain = itemgetter("x") | RunnableLambda(fake_llm)
+response = itemgetter_chain.invoke({"x": 20})
 console.print(response)
